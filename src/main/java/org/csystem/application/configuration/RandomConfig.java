7798c8b7f03d8b6fac92427bf71aec05d7f27e46
bolat <muhammetbolat@gmail.com>
2022-08-28 14:08:02 +0300

    unused packages are removed

--- src/main/java/org/csystem/application/configuration/RandomConfig.java
+++ src/main/java/org/csystem/application/configuration/RandomConfig.java
@@ -3,6 +3,7 @@ package org.csystem.application.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
 
 import java.util.Random;
 
